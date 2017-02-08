eb57192daf0d5d1a3b54332181930ca8e10c9cb5
PavelAlenichev <p.alenichev@gmail.com>
2017-02-08 16:10:29 +0200

    Update Main.java

--- DEV-1/source/Main.java
+++ DEV-1/source/Main.java
@@ -3,6 +3,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        Checker.checkInput(args);
+        Checker checker = new Checker();
+        checker.chexckInput(args);
     }
 }
