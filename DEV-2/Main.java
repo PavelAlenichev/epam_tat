5f4ac191fa3d5c75c898f77b67f575baf0b177a0
Pavel Alenichev <p.alenichev@gmail.com>
2017-02-13 16:16:01 +0300

    Task 2 without negative and exp

---
+++ DEV-2/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Entry point in application
+ * Creating exemplar of ReversedPolishNotation
+ * and give string with expression there
+ * (Class ReversedPolishNotation doesn't support negative numbers and exp view of numbers)
+ * Code is working as task asks, but can't process some exceptions like ^
+ */
+public class Main {
+    public static void main(String[] args) {
+        new ReversedPolishNotation("22 +2" );
+    }
+}
