641089f9cf51e451a224f54b1cdfc0b72e7805ea
PavelAlenichev <p.alenichev@gmail.com>
2017-02-18 23:50:28 +0300

    removed magic numbers in Main

--- DEV-4/src/Main.java
+++ DEV-4/src/Main.java
@@ -10,6 +10,8 @@ import stringHandlers.StringReader;
  */
 public class Main {
 
+  private static final int INTERUPT_NUMBER_OF_OPERATIONS = 3;
+
   /**
    * Entry point in application
    * uses infinity cycle
@@ -28,7 +30,8 @@ public class Main {
       receivedText = concatenator.concatenate(receivedText);
       StringParser parser = new StringParser(receivedText);
 
-      if (operationCounter > 3 && receivedText.equalsIgnoreCase("exit")) {
+      if (operationCounter > INTERUPT_NUMBER_OF_OPERATIONS
+          && receivedText.equalsIgnoreCase("exit")) {
         break;
       } else {
         outputer.printInConsole(new FrequencyCounter(parser.parse()));
