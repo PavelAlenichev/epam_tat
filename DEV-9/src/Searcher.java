dabca8fd221056adf34ed3cbe86f1bb4c1be3090
berthold <mhkyon@gmail.com>
2017-03-03 13:33:23 +0300

    realised method to search needed Values by Key

--- DEV-9/src/Searcher.java
+++ DEV-9/src/Searcher.java
@@ -5,12 +5,17 @@ import java.util.NoSuchElementException;
  */
 public class Searcher {
 
+  private final static String SECTION = "section";
+  private final static String KEY = "key";
+  private final static String NEW_LINE = "\n";
+
   public void searchSection(String file, String section, Keeper keeper){
 
     int firstOccurrence;
     int newSection;
     char sectionOpen = '[';
-    if (isExist(file,section)) {
+
+    if (isExist(file,section, SECTION)) {
       firstOccurrence = file.indexOf(section);
       newSection = file.indexOf(sectionOpen, firstOccurrence + 1);
       keeper.setSection(file.substring(firstOccurrence, newSection));
@@ -18,19 +23,31 @@ public class Searcher {
   }
 
   public void searchValues(String section, String key, Keeper keeper) {
+    key = key + "=";
 
-    if(isExist(section,key)) {
-
+    int index = 0;
+    int newLineIndex;
+    if(isExist(section,key, KEY)) {
+      index = section.indexOf(key, index);
+      if (index != section.lastIndexOf(key)) {
+        while (index != section.lastIndexOf(key)) {
+          newLineIndex = section.indexOf(NEW_LINE, index);
+          keeper.putInformation(section.substring(index + key.length() + 1, newLineIndex));
+          index++;
+        }
+      }
     }
 
+    if (section.indexOf(key) != section.lastIndexOf(key)) {
 
+    }
   }
 
-  private boolean isExist(String wholeString, String value){
+  private boolean isExist(String wholeString, String value, String method){
     if (wholeString.contains(value)) {
       return true;
     } else {
-      throw new NoSuchElementException();
+      throw new NoSuchElementException("No such " + method);
     }
   }
 
