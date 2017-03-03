d3c39057700d0892c63b296d5d5016e2089f7c0d
berthold <mhkyon@gmail.com>
2017-03-03 11:45:17 +0300

    added section reader in ConsoleDialog with logger

--- DEV-9/src/ConsoleDialog.java
+++ DEV-9/src/ConsoleDialog.java
@@ -1,6 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Created by User on 03.03.2017.
  */
 public class ConsoleDialog {
+
+  private static Logger log = Logger.getLogger(ConsoleDialog.class.getName());
+
+  public String readSection() {
+    String section = "";
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    int attempt = 10;
+    while (attempt > 0) {
+      try {
+        section = reader.readLine();
+        if (section.getClass().equals("class java.lang.String")) {
+          break;
+        }
+      } catch (IOException e) {
+        log.log(Level.SEVERE, "Exception: IOException", e);
+        continue;
+      }
+    }
+
+    return section;
+  }
 
 }
