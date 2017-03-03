42e5d472b3d08068acd3001fd53b80388fc695ab
berthold <mhkyon@gmail.com>
2017-03-03 11:21:15 +0300

    added abstract class Keeper to weak bound realised methods in DataKeeper

--- DEV-9/src/DataKeeper.java
+++ DEV-9/src/DataKeeper.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by User on 03.03.2017.
  */
-public class DataKeeper {
+public class DataKeeper extends Keeper {
 
+  private String textFromFile;
+  private ArrayList<String> values = new ArrayList<>();
+
+  public DataKeeper(String textFromFile) {
+    this.textFromFile = textFromFile;
+  }
+
+  @Override
+  public String getInformation() {
+    return this.textFromFile;
+  }
+
+  @Override
+  public void putInformation(String data) {
+    values.add(data);
+  }
 }
---
+++ DEV-9/src/Keeper.java
@@ -0,0 +1,10 @@
+/**
+ * Created by User on 03.03.2017.
+ */
+public abstract class Keeper {
+
+  abstract public String getInformation();
+
+  abstract public void putInformation(String data);
+
+}
