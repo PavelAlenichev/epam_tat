56cd17e02ca2ac41275ea3194ac81e6c88d04357
berthold <mhkyon@gmail.com>
2017-03-17 16:04:20 +0300

    added randomizer tests to define char intervals

--- DEV12/src/test/java/RandomizerTest.java
+++ DEV12/src/test/java/RandomizerTest.java
@@ -1,6 +1,31 @@
+import org.junit.Test;
+
 /**
  * Created by User on 17.03.2017.
  */
 public class RandomizerTest {
+
+  @Test
+  public void lowerCaseTests() {
+    RandomForExample randomForExample = new RandomForExample();
+
+    String s = randomForExample.generateLowerCaseRandomExample();
+
+    char first = s.charAt(0);
+    char second = s.charAt(1);
+
+    assert (first >= 97 && first <= 104 && second >= 1 && second <= 8);
+  }
+
+  public void upperCaseTests() {
+    RandomForExample randomForExample = new RandomForExample();
+
+    String s = randomForExample.generateUpperCaseRandomExample();
+
+    char first = s.charAt(0);
+    char second = s.charAt(1);
+
+    assert (first >= 65 && first <= 72 && second >= 1 && second <= 8);
+  }
 
 }
